90820ed228daaddbc8cdc96801f7e3ea4cc1771a
Ian Luites <IanLuites@users.noreply.github.com>
2020-11-02 02:38:26 +0100

    Add mix release tests.

---
+++ test_projects/mix_release/lib/adapterized.ex
@@ -0,0 +1,18 @@
+defmodule Adapterized do
+  @moduledoc """
+  Documentation for `Adapterized`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Adapterized.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test_projects/mix_release/mix.exs
@@ -0,0 +1,26 @@
+defmodule Adapterized.MixProject do
+  use Mix.Project
+
+  def project do
+    [
+      app: :adapterized,
+      version: "0.1.0",
+      elixir: "~> 1.10",
+      start_permanent: Mix.env() == :prod,
+      deps: deps()
+    ]
+  end
+
+  def application do
+    [
+      extra_applications: [:logger, :ex_unit],
+      mod: {Adapterized.Application, []}
+    ]
+  end
+
+  defp deps do
+    [
+      {:adapter, path: "../../"}
+    ]
+  end
+end
